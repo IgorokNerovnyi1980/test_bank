457608df2289883cc43e17c1b4f20be9e212b37a
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-10-01 15:14:06 +0300

    Updates

---
+++ precache-manifest.0682e488bb926639e8d4fd64a1a97190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c61e541b82463fd81c33a580ed0fe0d",
+    "url": "/test_bank/index.html"
+  },
+  {
+    "revision": "c2255e69c23c811b294d",
+    "url": "/test_bank/static/css/main.3397d155.chunk.css"
+  },
+  {
+    "revision": "f778b1df1f4620b6d1a4",
+    "url": "/test_bank/static/js/2.a138bd44.chunk.js"
+  },
+  {
+    "revision": "c2255e69c23c811b294d",
+    "url": "/test_bank/static/js/main.aca43bec.chunk.js"
+  },
+  {
+    "revision": "fe66097607f5b448e40d",
+    "url": "/test_bank/static/js/runtime-main.a37fb491.js"
+  },
+  {
+    "revision": "03a82aca4de28be1a1869b8ade7774ea",
+    "url": "/test_bank/static/media/card.03a82aca.png"
+  },
+  {
+    "revision": "ec415a7556f8048ae2b027385bb53a83",
+    "url": "/test_bank/static/media/ezgif_2.ec415a75.png"
+  }
+]);
